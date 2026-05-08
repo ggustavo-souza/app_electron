bccbbb1eee9ea33fad63fa399f20e88d43febcbb
ggustavo-souza <cavalogames1231@gmail.com>
2026-05-08 16:00:27 -0300

    fix(database): adicionando o "migrador" para executar as migrations necessárias assim que o banco for executado.

--- src/main/db.ts
+++ src/main/db.ts
@@ -3,13 +3,22 @@ import path from 'path';
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import * as schema from './schema';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // salvar a database no appData do usuário
 const dbPath = path.join(app.getPath('userData'), 'database.sqlite');
+console.log("banco criado em: " + dbPath);
 // cria a conexao com o banco de dados
 const sqlite = new Database(dbPath);
 // exporta o banco de dados
 export const db = drizzle(sqlite, { schema });
+
+migrate(db, { migrationsFolder: path.join(__dirname, '../drizzle') })
+
 
 
 
