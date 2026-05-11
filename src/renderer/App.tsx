c837ceaf8de7ad4292deaa763c4db7d7bd9e7f3c
ggustavo-souza <cavalogames1231@gmail.com>
2026-05-11 15:59:49 -0300

    fix: consertando nomeação referente às classes de cor de fundo do css

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,8 +11,8 @@ function App() {
     <>
       <main className='m-12'>
         <div className='flex gap-2'>
-          <button className='primaryBackground border-none font-bold py-2 px-6 rounded' onClick={() => setCurrentView('tasks')}>Tarefas</button>
-          <button className='primaryBackground border-none font-bold py-2 px-6 rounded mx-4 gap-2 flex gap-2' onClick={() => setCurrentView('settings')}><i className='bi bi-gear-fill font-bold'></i>Configurações</button>
+          <button className='secondaryBackground border-none font-bold py-2 px-6 rounded' onClick={() => setCurrentView('tasks')}>Tarefas</button>
+          <button className='secondaryBackground border-none font-bold py-2 px-6 rounded mx-4 gap-2 flex gap-2' onClick={() => setCurrentView('settings')}><i className='bi bi-gear-fill font-bold'></i>Configurações</button>
         </div>
         <div>
           {currentView === 'tasks' && <Tarefas />}
