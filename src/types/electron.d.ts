bd17ce31dd43ff1cc6729ca80d9f2f1ce88aa093
ggustavo-souza <cavalogames1231@gmail.com>
2026-05-18 14:25:55 -0300

    feat: registrando as novas funcoes de dark mode no type geral do projeto (para evitar problemas com o typescript)

--- src/types/electron.d.ts
+++ src/types/electron.d.ts
@@ -7,6 +7,11 @@ declare global {
             criarTarefa: (titulo: string, descricao: string) => Promise<void>;
             marcarComoConcluida: (id: number, concluida: boolean) => Promise<void>;
             excluirTarefa: (id: number) => Promise<void>;
+            onThemeChange: (callback: (isDark: boolean) => void) => () => void; // responsavel por atualizar o tema do front-end
+        }
+        darkMode: {
+            toggle: () => Promise<boolean>; // responsavel por alternar o dark mode
+            isDark: () => Promise<boolean>; // responsavel por verificar se o dark mode está ativado
         }
     }
 }
