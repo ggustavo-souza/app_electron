526af2e63dccec77dc9ae6e0c0c9dede60ac426f
ggustavo-souza <cavalogames1231@gmail.com>
2026-05-18 14:23:15 -0300

    feat: criada a ponte no preload para receber a informação referente ao dark mode vinda do front-end, onde recebe o evento e o callback (que no caso é o responsável por trocar o tema) + expoẽ as funções do dark mode

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -25,4 +25,19 @@ contextBridge.exposeInMainWorld('api', {
   criarTarefa: (titulo: string, descricao: string) => ipcRenderer.invoke('criar-tarefa', titulo, descricao),
   marcarComoConcluida: (id: number, concluida: boolean) => ipcRenderer.invoke('marcar-como-concluida', id, concluida),
   excluirTarefa: (id: number) => ipcRenderer.invoke('excluir-tarefa', id),
+  // recebe a informação do dark mode do front-end para atualizar o tema
+  onThemeChange: (callback: (isDark: boolean) => void) => {
+    // aqui o listener é uma função que recebe o evento e o callback, e retorna o callback que no caso é o isDark(função responsável por atualizar o tema do front-end)
+    // o _event é uma variável que não será usada
+    const listener = (_event: Electron.IpcRendererEvent, isDark: boolean) => callback(isDark)
+    ipcRenderer.on('theme-changed', listener)
+    //remove o listener quando o componente for desmontado
+    return () => ipcRenderer.removeListener('theme-changed', listener)
+  }
+})
+// expoe um objeto que possui duas funções, uma para alternar o dark mode e outra para verificar se o dark mode está ativado
+// ambas as funções são chamadas pelo front-end
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  isDark: () => ipcRenderer.invoke('dark-mode:is-dark')
 })
